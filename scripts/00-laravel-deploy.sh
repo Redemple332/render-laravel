c184069e3f78e4b934a5561b97634e53813ac575
RedempleDev <redemple.marcelo@redcoresolutions.com>
2024-01-13 13:40:19 +0800

    key generate

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,11 +3,14 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Running Key Generate"
+php artisan key:generate
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
 
 # echo "Running migrations..."
 # php artisan migrate --force
